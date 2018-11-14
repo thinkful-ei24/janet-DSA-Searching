2514da1ba798cc47ff552fd554469a4417e42ed9
Janet Leon <leon.irenejanet@gmail.com>
2018-11-14 08:48:16 -0800

    linear search button works

--- src/App.js
+++ src/App.js
@@ -5,18 +5,25 @@ import './App.css';
 class App extends Component {
 constructor(props){
   super(props);
+  this.input = React.createRef();
   this.state={
     count: 0,
     target:0
-  };
+  }
+
+const linearSearch = ()=>{
+  this.setState({})
 }
+}
+
+
 
   render() {
     return (
       <section className="App">
         <h1>Type Data Here</h1>
-       <input type='text'></input>
-       <button type= 'button'>Linear Search</button>
+       <input type='text' ref={this.input}></input>
+       <button type= 'button' onClick = {(e)=>console.log(this.input.current.value)}>Linear Search</button>
        <div>It took {this.state.count} times to find {this.state.target}</div>
       </section>
     );
